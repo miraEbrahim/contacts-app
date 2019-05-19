c24ec2e2167352c329ba0e5dedfca25ccbc34c53
Maryam Ebrahim <mxebrahim@gmail.com>
2019-05-19 23:25:36 +0100

    added contacts details

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -5,7 +5,12 @@ class ListContacts extends Component {
         return(
             <ol className='contact-list'>
                 {this.props.contacts.map((contact) =>(
-                    <li key="{contact.name}"> {contact.name}</li>
+                    <li key="{contact.name}"> {contact.name}
+                    <div className='contact-list-details'>
+                    <p>{contact.phone_number}</p>
+                    <p>{contact.address}</p>
+                    </div>
+                    </li>
                 ) )}
             </ol>
         )
